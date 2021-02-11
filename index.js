33b3b07862b0f23af5c6ce279d9ce617e6d1ad16
Sunitha Vibaker <sunitha.vibaker@provise.com>
2021-02-11 16:46:20 +0530

    first commit

--- index.js
+++ index.js
@@ -7,33 +7,10 @@ const app = express();
 // Setup server port
 const port = process.env.PORT || 4000;
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
-
-// parse requests of content-type - application/json
-app.use(bodyParser.json())
-
-// Configuring the database
-const dbConfig = require('./config/db.config.js');
-const mongoose = require('mongoose');
-
-mongoose.Promise = global.Promise;
-
-// Connecting to the database
-mongoose.connect(dbConfig.url, {
-    useNewUrlParser: true
-}).then(() => {
-    console.log("Successfully connected to the database");    
-}).catch(err => {
-    console.log('Could not connect to the database.', err);
-    process.exit();
-});
-
-
-// Require Users routes
-const deviceRoutes = require('./src/routes/device.routes')
 // using as middleware
-app.use('/devices', deviceRoutes)
+app.use('/', function(req,res){
+  res.send("hello")
+})
 
 // listen for requests
 app.listen(port, () => {
